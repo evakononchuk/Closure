9d4af766b12241fd7c0a40a655e8a7da52138ffa
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-08-15 01:02:12 +0300

    repo with tasks #5

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
-'use strict';
+"use strict";
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const element = (i) => arr[i];
+  element.push = (x) => arr.push(x);
+  element.pop = () => arr.pop();
+  return element;
+};
 
 module.exports = { array };
